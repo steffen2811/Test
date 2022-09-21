ee508bce66ca1b00ad37405907824c04d9f3dd1d
Steffen K. Thomsen <steffen2811@live.dk>
2022-09-21 21:11:29 +0200

    Add exercise 2 (week 3)#

---
+++ Exercise_2/TestTransportPrice/TransportPriceTestInlineData.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using TransportPrice;
+
+namespace TestTransportPrice
+{
+    public class TransportPriceTestInlineData
+    {
+        [Theory]
+        [InlineData(9.99, 4.95, 0)]
+        [InlineData(10, 4.9999, 50)]
+        [InlineData(9.99, 5, 75)]
+        [InlineData(10, 5, 100)]
+        public void TestTransportPrice(double weight, double distance, double expectedResult)
+        {
+            var transportPrice = new TransportPriceClass();
+            double result = transportPrice.GetPrice(weight, distance);
+
+            Assert.Equal(expectedResult, result);
+        }
+    }
+}
---
+++ Exercise_2/TransportPrice/TransportPrice.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TransportPrice
+{
+    public class TransportPriceClass
+    {
+
+        public double GetPrice(double weight, double distance)
+        {
+            if (distance < 5)
+            {
+                if (weight < 10)
+                    return 0;
+                else
+                    return 50;
+            }
+            else // (distance >= 5)
+            {
+                if (weight < 10)
+                    return 75;
+                else
+                    return 100;
+            }
+        }
+
+    }
+}
