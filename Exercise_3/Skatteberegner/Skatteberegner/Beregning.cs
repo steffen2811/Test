4a27c7f47f805d8b933781348f2629f16184b2cf
Steffen K. Thomsen <steffen2811@live.dk>
2022-10-09 21:03:23 +0200

    Move Skatteberegneren to main project.

---
+++ Exercise_3/Skatteberegner/Skatteberegner/Beregning.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Skatteberegner
+{
+    public class Beregning
+    {
+        // Funktionen returnerer det beløb, der skal beskattes (altså ikke selve skatten).
+        public double SkatVedJulegave(double julegave, double andenGave)
+        {
+            double beskatning;
+
+            if ((julegave + andenGave) > 1200)
+            {
+                if (julegave <= 900)
+                    beskatning = andenGave;
+                else
+                    beskatning = julegave + andenGave;
+            }
+            else
+                beskatning = 0;
+
+            return beskatning;
+        }
+    }
+}
